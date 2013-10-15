7025fb0c71d442fc282d87f5be2cdeb252fea441
zeppelin <gabor.babicz@gmail.com>
2013-10-15 17:06:17 +0200

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,24 @@
+import Resolver from 'resolver';
+import registerComponents from 'appkit/utils/register_components';
+
+Ember.ENV.ENABLE_ALL_FEATURES = true;
+
+var App = Ember.Application.extend({
+  LOG_ACTIVE_GENERATION: true,
+  LOG_MODULE_RESOLVER: true,
+  LOG_TRANSITIONS: true,
+  LOG_TRANSITIONS_INTERNAL: true,
+  LOG_VIEW_LOOKUPS: true,
+  modulePrefix: 'appkit', // TODO: loaded via config
+  Resolver: Resolver
+});
+
+App.initializer({
+  name: 'Register Components',
+  initialize: function(container, application) {
+    registerComponents(container);
+  }
+});
+
+
+export default App;
